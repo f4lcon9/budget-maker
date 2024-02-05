b6819d052f395136a4eeba8552e51763658377dd
f4lcon9 <productsvkgaming@gmail.com>
2024-02-05 20:27:15 +0530

    Delete route.ts

--- app/api/delete/route.ts
+++
@@ -1,7 +0,0 @@
-import Data from "@/model/data"
-import { NextResponse } from "next/server"
-
-export async function GET(request: Request) {
-    const data = await Data.deleteMany()
-    return NextResponse.json("HI")
-}
